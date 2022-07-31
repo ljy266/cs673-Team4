f55ecb497b36ef836e925f44776886c3fe1fc773
Jiyuan Liu <81460575+ljy266@users.noreply.github.com>
2022-07-31 02:31:44 -0400

    Add files via upload

--- routes/items.js
+++ routes/items.js
@@ -3,19 +3,45 @@ const router = express.Router()
 const Item = require('../models/item')
 
 //All items route
-router.get('/', (req,res) => {
-    res.render('items/index')
+router.get('/', async (req, res) => {
+    let searchOptions = {}
+    if (req.query.name != null && req.query.name !== '') {
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try {
+        const items = await Item.find(searchOptions)
+        res.render('items/index', {
+            items: items,
+            searchOptions: req.query
+        })
+    } catch {
+        res.redirect('/')
+    }
 })
 
 
 //new item Route
-router.get('/new',(req, res) => {
-    res.render('items/new', {item: new Item()})
+router.get('/new', (req, res) => {
+    res.render('items/new', { item: new Item() })
 })
 
 //create item Route
-router.post('/', (req, res) =>{
-    res.send('Create')
+router.post('/', async (req, res) => {
+    const item = new Item({
+        name: req.body.name
+    })
+
+    try {
+        const newItem = await item.save()
+        //res.redirect(`items/${newItem.id}`)
+        res.redirect(`items`)
+    } catch {
+        res.render('items/new', {
+            item: item,
+            errorMessage: 'Error createing Item'
+        })
+    }
 })
+
 
 module.exports = router
